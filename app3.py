3cb39c9c8161b1ee91c8516fc7d32e087f97d4b9
Santacecilia <jorgesantacecilia@gmail.com>
2024-02-23 09:18:48 -0300

    9

--- app3.py
+++ app3.py
@@ -404,7 +404,7 @@ def app4():
     #   dol = float(827) #value2
     #else:
     #    print("Failed to retrieve data")
-    dol = float(837)
+    dol = float(838)
     left.metric("Dolar mayorista", '${:,}'.format(float(dol)))
      
 #SELECCIÓN DEPARTAMENTE Y PROVINCIA (INICIALIZACION)
